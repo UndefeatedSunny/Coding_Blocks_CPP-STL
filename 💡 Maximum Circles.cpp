d748ad5afaea5d10a1980e35581abd885e1731f3
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-07-12 15:56:30 +0530

    Update 💡 Maximum Circles.cpp

--- 💡 Maximum Circles.cpp
+++ 💡 Maximum Circles.cpp
@@ -1,34 +1,36 @@
 #include <bits/stdc++.h>
-#define ll long long int
-#define FAST_IO ios::sync_with_stdio(false); cin.tie(0);cout.tie(0)
 using namespace std;
 
- 
-int main() 
-{
-	FAST_IO;
-	ll n;
-	cin>>n;
-	unordered_map<ll,ll> m;
-	ll val;
-	for(ll i=0;i<n;i++)
-	{
-	    cin>>val;
-	    m[val]++;
+bool comp(pair<int,int> a,pair<int,int> b){
+    if((a.first+a.second)==(b.first+b.second)){
+		return (a.first-a.second)<(b.first-b.second);
+	}else{
+		return (a.first+a.second)<(b.first+b.second);
 	}
-	ll max=0;
-	ll pos=0;
-	
-	unordered_map<ll,ll>::iterator p;
-	
-	for(p=m.begin();p!=m.end();p++)
-	{
-	    if((*p).second>max)
-	    {
-	        pos=(*p).first;
-	        max=(*p).second;
-	    }
-	}
-	cout<<pos;
-	return 0;
+}
+int main() {
+    int ans = 1;
+    int n,c,r;
+    cin>>n;
+    vector< pair<int,int> > v;
+
+    for(int i=0;i<n;i++){
+        cin>>c>>r;
+        v.push_back(make_pair(c,r));
+    }
+
+sort(v.begin(),v.end(),comp);
+
+int val = v[0].first+v[0].second;
+
+for(int i=0;i<n-1;i++){
+    if(val <= v[i+1].first-v[i+1].second){
+        ans++;
+        val = v[i+1].first+v[i+1].second;
+    }
+
+}
+
+cout<<n-ans;
+
 }
